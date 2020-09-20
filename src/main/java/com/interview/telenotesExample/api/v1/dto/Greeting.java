5038d4da73230dde8b252b3e30ba4c6062480948
ryan.waggoner <ryan.waggoner@keap.com>
2020-09-19 22:05:39 -0600

    initial commit

---
+++ src/main/java/com/interview/telenotesExample/api/v1/dto/Greeting.java
@@ -0,0 +1,20 @@
+package com.interview.telenotesExample.api.v1.dto;
+
+public class Greeting {
+
+  private final long id;
+  private final String content;
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
---
+++ src/test/java/com/interview/telenotesExample/TelenotesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.telenotesExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelenotesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
